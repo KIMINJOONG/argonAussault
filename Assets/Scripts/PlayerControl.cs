e35b2d547393ef14e3bf1b43e7fa240c0e5da974
KimInJoong <ijboym216@gmail.com>
2025-01-30 18:15:35 +0900

    FEAT:Input으로 y,x이동

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -3,6 +3,7 @@ using UnityEngine.InputSystem;
 
 public class PlayerControl : MonoBehaviour
 {
+    [SerializeField] float controlSpeed = 10f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -21,8 +22,12 @@ public class PlayerControl : MonoBehaviour
         float xThrow = Input.GetAxis("Horizontal");
         float yThrow = Input.GetAxis("Vertical");
 
-        float xOffset = .1f;
+        float xOffset = xThrow * Time.deltaTime * controlSpeed;
         float newXPos = transform.localPosition.x + xOffset;
-        transform.localPosition = new Vector3(newXPos, transform.localPosition.y, transform.localPosition.z);
+
+
+        float yOffset = yThrow * Time.deltaTime * controlSpeed;
+        float newYPos = transform.localPosition.y + yOffset;
+        transform.localPosition = new Vector3(newXPos, newYPos, transform.localPosition.z);
     }
 }
